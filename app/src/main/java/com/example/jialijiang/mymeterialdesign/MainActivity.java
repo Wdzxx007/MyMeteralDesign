2793bc0f82f806c2b781c4d399ad20bf389639b6
Star <1053522020@qq.com>
2017-12-26 14:27:13 +0800

    添加权限问题，打电话

--- app/src/main/java/com/example/jialijiang/mymeterialdesign/MainActivity.java
+++ app/src/main/java/com/example/jialijiang/mymeterialdesign/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.jialijiang.mymeterialdesign;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,6 +21,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -42,6 +49,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView imgSearch;
     private GirlEntity myGirlEntity;
     private SwipeRefreshLayout mRefreshLayout;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1;
     private GirlEntity[] girls = {new GirlEntity("AngleBaby", R.drawable.angle), new GirlEntity("古力娜扎", R.drawable.gulinazha),
             new GirlEntity("林允儿", R.drawable.linyuner), new GirlEntity("刘亦菲", R.drawable.liuyifei),
             new GirlEntity("孙俪", R.drawable.suili), new GirlEntity("佟丽娅", R.drawable.tongliya),
@@ -104,7 +112,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //                snackbar.show();
                 switch (item.getItemId()) {
                     case R.id.nav_call:
-                        Toast.makeText(MainActivity.this, "妹子被点击了", Toast.LENGTH_SHORT).show();
+                        // 检查是否获得了权限（Android6.0运行时权限）
+                      if( ContextCompat.checkSelfPermission( MainActivity.this, android.Manifest.permission.CALL_PHONE )!= PackageManager.PERMISSION_GRANTED ) {
+                          // 没有获得授权，申请授权
+                          if( ActivityCompat.shouldShowRequestPermissionRationale( MainActivity.this,
+                                                                                   Manifest.permission.CALL_PHONE ) ){
+                              // 返回值：
+                              //                          如果app之前请求过该权限,被用户拒绝, 这个方法就会返回true.
+                              //                          如果用户之前拒绝权限的时候勾选了对话框中”Don’t ask again”的选项,那么这个方法会返回false.
+                              //                          如果设备策略禁止应用拥有这条权限, 这个方法也返回false.
+                              // 弹窗需要解释为何需要该权限，再次请求授权
+                              Toast.makeText(MainActivity.this, "请授权！", Toast.LENGTH_LONG).show();
+
+                              // 帮跳转到该应用的设置界面，让用户手动授权
+                              Intent intent = new Intent( Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                              Uri uri = Uri.fromParts( "package", getPackageName(), null);
+                              intent.setData(uri);
+                              startActivity(intent);
+                          }else {
+                              // 不需要解释为何需要该权限，直接请求授权
+                              ActivityCompat.requestPermissions(MainActivity.this,
+                                                                new String[]{Manifest.permission.CALL_PHONE},
+                                                                MY_PERMISSIONS_REQUEST_CALL_PHONE);
+                          }
+                      }else {
+                          // 已经获得授权，可以打电话
+                          CallPhone();
+                      }
                         break;
                     case R.id.nav_friends:
                         Toast.makeText(MainActivity.this, "段子被点击了", Toast.LENGTH_SHORT).show();
@@ -237,5 +271,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             finish();
             System.exit(0);
         }
+    }
+    private void CallPhone() {
+        String number = "18314834450";
+        if ( TextUtils.isEmpty( number)) {
+            // 提醒用户
+            // 注意：在这个匿名内部类中如果用this则表示是View.OnClickListener类的对象，
+            // 所以必须用MainActivity.this来指定上下文环境。
+            Toast.makeText(MainActivity.this, "号码不能为空！", Toast.LENGTH_SHORT).show();
+        } else {
+            // 拨号：激活系统的拨号组件
+            Intent intent = new Intent(); // 意图对象：动作 + 数据
+            intent.setAction(Intent.ACTION_CALL); // 设置动作
+            Uri data = Uri.parse("tel:" + number); // 设置数据
+            intent.setData(data);
+            startActivity(intent); // 激活Activity组件
+        }
+    }
+    // 处理权限申请的回调
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_CALL_PHONE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 授权成功，继续打电话
+                    CallPhone();
+                } else {
+                    // 授权失败！
+                    Toast.makeText(this, "授权失败！", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+
     }
 }
